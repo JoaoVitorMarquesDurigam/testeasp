3c922010580b2476159dbbe5393712d91cd02dda
nascimento <gustaveiro27@gmail.com>
2025-05-23 10:40:18 -0300

    primeiro commit

---
+++ Models/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿namespace teste1.Models
+{
+    public class Pessoa
+    {
+        /* o get é um metodo que retorna o valor atual de um atrobuto 
+         O set permite definir ou modificar o valor de um atributo */ 
+        public int PessoaId { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
