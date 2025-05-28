21ba2919daf4c91a33dea8b7483cbaf2fcd92a52
João Vitor Marques Durigam <joaovitordurigam@gmail.com>
2025-05-28 09:24:31 -0300

    Correção de usuario e codigo

--- obj/Debug/net8.0/teste1.AssemblyInfo.cs
+++ obj/Debug/net8.0/teste1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("teste1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c922010580b2476159dbbe5393712d91cd02dda")]
 [assembly: System.Reflection.AssemblyProductAttribute("teste1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("teste1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
